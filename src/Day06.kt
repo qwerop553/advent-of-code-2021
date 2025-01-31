dbdd2376e152c3104cb29d8fd88fc3ec63cc153a
dan <qwerop553@gmail.com>
2025-01-31 14:52:57 +0900

    나무를 심습니다.

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,13 +1,13 @@
 import java.math.BigInteger
+import kotlin.math.pow
 
-fun main(){
-
+fun main() {
 
 
     fun part1(input: List<String>, days: Int): BigInteger {
         var fishes = input.first().split(",").map { it.toInt() }.sorted().groupingBy { it }.eachCount().toMutableMap()
         val fihes: MutableMap<Int, BigInteger> = mutableMapOf()
-        for ((k, v) in fishes){
+        for ((k, v) in fishes) {
             fihes[k] = BigInteger(v.toString())
         }
         for (i in 1..days) {
@@ -17,19 +17,29 @@ fun main(){
             for (day in 0..7) {
                 fihes[day] = fihes[day + 1] ?: BigInteger("0")
             }
-            fihes[6] = fihes[6]!!  + newborns
+            fihes[6] = fihes[6]!! + newborns
             fihes[8] = newborns
         }
 
         var sum = BigInteger("0")
-        for(a in fihes.values) sum += a
+        for (a in fihes.values) sum += a
+        return sum
+    }
+
+    fun part2(input: List<String>): BigInteger? {
+        val fishes = input.first().split(",").map { it.toInt() }.sorted().groupingBy { it }.eachCount()
+        var sum = BigInteger.ZERO
+        for (key in 0..8)
+            sum += (fishes.getOrDefault(0, 0) * (2.0.pow(((80 - key) / 7)))).toInt().toBigInteger()
+
         return sum
     }
 
 
 
-    //assert(part1(readInput("Day06_test"), 80)==5934)
-    println(part1(readInput("Day06"), 256))
+
+    check(part2(readInput("Day06_test"))==BigInteger("5934"))
+    //println(part1(readInput("Day06"), 256))
 
 
 }
